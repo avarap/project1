6a0dd2499f73be92275b20c102fb8d07230ea815
varaa <avarap@gmail.com>
2022-08-20 15:01:30 +0200

    commit 2022-08-20

--- js/index.js
+++ js/index.js
@@ -1,9 +1,9 @@
 let isLeft;
 let isRight;
 let isFalling;
-let isPlummeting;
+let overPlatform;
 let isJumping;
-let platformPos_y;
+//let platformPos_y;
 
 let game_score;
 let lives;
@@ -14,14 +14,15 @@ let house;
 let player;
 const platforms = [new Platform()];
 
-const keys = { right: { pressed: false }, left: { pressed: false } };
+const keys = {
+  right: { pressed: false },
+  left: { pressed: false },
+  up: { pressed: false },
+};
 
 function preload() {}
 
 function setup() {
-  createCanvas(1024, 576);
-  player = new Player();
-  //platforms = new Platform();
   startGame();
 }
 
@@ -32,62 +33,49 @@ function setup() {
 function keyPressed() {
   //console.log(keyCode);
   switch (keyCode) {
-    case 38:
-      //   console.log("up");
+    case 38: //up
+      
       //problem: jump is accumulating
-    //   if (player.velocity.y >= -20) {
-        player.velocity.y -= 20;
-        // console.log(player.velocity);
-    //   } else console.log("reached", player.velocity);
-
+      if (!keys.up.pressed)
+      player.velocity.y -= 15;
+      keys.up.pressed = true;
       break;
-    case 40:
-      //   console.log("down");
+    case 40: //down
       break;
-    case 37:
-      //   console.log("left");
+    case 37: //left
       keys.left.pressed = true;
       break;
-    case 39:
-      //console.log("right");
+    case 39: //right
       keys.right.pressed = true;
       break;
-    case 32:
-      //   console.log("space");
+    case 32: //space
       break;
   }
-  console.log(keys.right.pressed);
 }
 
 function keyReleased() {
   switch (keyCode) {
-    case 38:
-      //console.log("up");
-      //problem: jump is accumulating
-      //if (player.velocity.y >= -10) {
-      player.velocity.y += 20;
-      //  console.log(player.velocity);
-      //} else console.log("reached", player.velocity);
-
+    case 38: //up
+      keys.up.pressed = false;
       break;
-    case 40:
-      //console.log("down");
+    case 40: //down
       break;
-    case 37:
-      //   console.log("left");
+    case 37: //left
       keys.left.pressed = false;
       break;
-    case 39:
-      //console.log("right");
+    case 39: //right
       keys.right.pressed = false;
       break;
-    case 32:
-      //console.log("space");
+    case 32: //space
       break;
   }
 }
 
-function startGame() {}
+function startGame() {
+  createCanvas(1024, 576);
+  player = new Player();
+
+}
 
 function draw() {
   background(color(100, 155, 255));
@@ -116,12 +104,31 @@ function draw() {
   platforms.forEach((platform) => {
     if (
       player.position.y + player.height <= platform.position.y &&
-      player.position.y + player.height + player.velocity.y >=
-        platform.position.y &&
+      player.position.y + player.height + player.velocity.y >= platform.position.y && //
       player.position.x + player.width >= platform.position.x &&
       player.position.x <= platform.position.x + platform.width
     ) {
       player.velocity.y = 0;
+      platform.onPlatform=true;
+    }else{
+      platform.onPlatform=false;
     }
   });
+
+  // overPlatform = platforms.reduce((sumT,platform)=>{
+  //   if(platform.onPlatform){
+  //     console.log("hola");
+  //     sumT+=1;
+  //   }
+  // },0);
+
+  const p = platforms.filter((platform) => platform.onPlatform==true);
+  if (p.length>=1)
+  {
+    overPlatform = 1;
+  }else{
+    overPlatform = 0;
+  }
+
+  //console.log(overPlatform);
 }
--- js/platform.js
+++ js/platform.js
@@ -4,22 +4,11 @@ class Platform{
         this.velocity = {x: 0, y: 0}
         this.width = 200;
         this.height = 20;
+        this.color = "blue";
+        this.onPlatform=false;
     }
     draw(){
-        fill("green");
+        fill(this.color);
         rect(this.position.x, this.position.y, this.width, this.height);//, 20, 20, 5, 5
     }
-    // update(){
-    //     this.draw();
-        
-    //     this.position.x +=this.velocity.x;
-    //     this.position.y +=this.velocity.y;
-
-    //     if (this.position.y + this.height + this.velocity.y <= height)
-    //     this.velocity.y += this.gravity;
-    //     else {
-    //         this.velocity.y = 0;
-    //         // console.log(this.position)
-    //     }
-    // }
 }
